ea2a838da986d5f7fcf8af570f1c9446b1e51884
ranfyy <ranfyy@gmail.com>
2018-03-04 15:15:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
-<pre>
 <?php
 include_once( "curl.inc.php" );
 include('lib/forecast.io.php');
+
+if( PHP_SAPI != "cli" ) {
+    print "
+<pre>
+";
+}
 
 $set = parse_ini_file( "secret.ini", true );
 
 $lat = $set["loc"]["lat"];
 $lon = $set["loc"]["lon"];
 $units = 'auto';  // Can be set to 'us', 'si', 'ca', 'uk' or 'auto' (see forecast.io API); default is auto
-$lang = 'de'; // Can be set to 'en', 'de', 'pl', 'es', 'fr', 'it', 'tet' or 'x-pig-latin' (see forecast.io API); default is 'en'
+$lang = 'en'; // Can be set to 'en', 'de', 'pl', 'es', 'fr', 'it', 'tet' or 'x-pig-latin' (see forecast.io API); default is 'en'
 
 $forecast = new ForecastIO(
   $set["api"]["key"]
@@ -43,20 +48,44 @@ if( false ) {
     }
 } // if
 
-print "
 /*
+print "
  * GET DAILY CONDITIONS FOR NEXT 7 DAYS
- */
 ";
+ */
+
 $conditions_week = $forecast->getForecastWeek($lat, $lon);
 
 echo "\n\nConditions this week:\n";
+
+$ahead = array(
+"date DayOfWeek :",
+"T max",
+"T min",
+"precip",
+"precip %",
+"summary",
+);
+
+$sep = PHP_EOL . "\t";
+$line = implode( $sep, $ahead );
+//print $line . PHP_EOL;
+
 foreach($conditions_week as $conditions) {
     $dow = $conditions->getTime('D');
-    echo $conditions->getTime('Y-m-d') . " " . $dow . ': ' 
-    . $conditions->getMaxTemperature()		." "
-    . $conditions->getPrecipitationType()	." "
-    . "\n";
+    if( ! ($prec = $conditions->getPrecipitationType()) ) {
+	     $prec = "-";
+    }
+    $probab = $conditions->getPrecipitationProbability() * 100;
+    print ""
+    . $conditions->getTime('Y-m-d') . " "
+    . $dow . ': '	. $sep
+    . $conditions->getMaxTemperature()	. $sep
+    . $conditions->getMinTemperature()	. $sep
+    . $prec   . $sep
+    . $probab	. " %" . $sep
+    . $conditions->getSummary()	. $sep
+    ;
 
 }
 
@@ -69,4 +98,3 @@ if( false ) {
     $condition = $forecast->getHistoricalConditions($lat, $lon, '2010-10-10T14:00:00-0700');
     echo "\n\nTemperatur 2010-10-10: ". $condition->getMaxTemperature(). "\n";
 } // if
-
